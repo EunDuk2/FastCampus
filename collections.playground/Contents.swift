81eb5185b20b3914918e95431b59e8e3b3bbce65
eunsung <dmsqls6789@naver.com>
2023-08-10 16:29:52 +0900

    Part1 - 4 - 2

--- collections.playground/Contents.swift
+++ collections.playground/Contents.swift
@@ -40,3 +40,63 @@ var primes: Set<Int> = [2, 3, 5, 7]
 var evens = Set<Int>([2, 4, 6, 8, 2, 4])
 
 var emptySet: Set<Int> = []
+
+
+
+/// tuple - 여러 데이터를 하나의 값으로 표현, 세부 데이터 접근시, 포지션 또는 이름으로 접근 가능
+var phone = (os:"iOS", model: "iPhone13")
+
+phone.0 // "iOS"
+phone.os // "iOS"
+
+var rawPhone = ("iOS", "iPhone13") // 얘는 순서로만 접근 가능
+
+
+/// enum 열거형 - 서로 관계있는 값들을 모아서 표현한 것, 특정 타입들을 표시할 때 사용하기 좋음
+// 요일을 enum으로
+enum WeekDay {
+    case mon
+    case tue
+    case wed
+    case thu
+    case fri
+}
+
+var today: WeekDay = .mon
+
+// 미디어타입을 enum으로
+//enum MediaType {
+//    case audio
+//    case video
+//}
+//
+//var mediaType: MediaType = .audio
+
+
+// 위에서 만들어본 타입에, 파일 확장자도 문자열로 받을 수 있게 수정
+
+enum MediaType {
+    case audio(String)
+    case video(String)
+}
+
+var mp3: MediaType = .audio("mp3")
+var h264: MediaType = .video("h264")
+
+// enum을 표시할 때 value를 할당해서 표시해야할 때도 있음
+enum MasterLevel: Int {
+    case beginner
+    case intermediate
+    case professional
+}
+
+let pro = MasterLevel(rawValue: 2)
+let otherLevel = MasterLevel(rawValue: 5) // -> nil
+
+enum Direction: String {
+    case up
+    case down = "ddd"
+}
+
+let up = Direction(rawValue: "up")
+let otherDirection = Direction(rawValue: "left") // -> nil
