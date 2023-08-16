9cd524ea0ed52c230ba12b23dfac256f5854b371
eunsung <dmsqls6789@naver.com>
2023-08-16 18:27:14 +0900

    Part1 - 7 - 2

--- functions.playground/Contents.swift
+++ functions.playground/Contents.swift
@@ -38,3 +38,57 @@ func printName(_ name: String) {
 }
 
 printName("Jason") // "name is Jason"
+
+
+func discount(price: Double, ratio: Double = 0.2) -> Double {
+    return price * (1 - ratio)
+}
+
+let defaultRatioApplied = discount(price: 2000)
+print(defaultRatioApplied)  // 1600
+
+let customRatioApplied = discount(price: 2000, ratio: 0.5)
+print(customRatioApplied)  // 1000
+
+func printNames(_ names: String...) {
+    for name in names {
+        print("name is \(name)")
+    }
+}
+
+printNames("James", "Roy", "Jake")
+// name is James
+// name is Roy
+// name is Jake
+
+
+enum DivideError: Error {
+    case cannotZero
+}
+
+func divide(dividend: Int, divisor: Int) throws -> Int {
+    if divisor == 0 {
+        throw DivideError.cannotZero
+    }
+    
+    return Int(dividend / divisor)
+}
+
+
+do {
+    let result = try divide(dividend: 80, divisor: 6)
+    // let result = try divide(dividend: 80, divisor: 0)
+    print(result)
+} catch {
+    print(error.localizedDescription)
+}
+
+
+func makeTriple(num: inout Int) {
+    num *= 3
+}
+
+var num = 8
+makeTriple(num: &num)
+
+print(num)
