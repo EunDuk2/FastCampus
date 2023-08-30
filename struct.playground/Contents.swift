8dee98d31be1cee93a6f20c580c805b8100efca5
eunsung <dmsqls6789@naver.com>
2023-08-30 16:33:03 +0900

    Part1 - 9 - 2

--- struct.playground/Contents.swift
+++ struct.playground/Contents.swift
@@ -1,12 +1,58 @@
 import UIKit
 
-struct Movie {
+//struct Movie {
+//    var name: String
+//}
+//
+//
+//var movie = Movie(name: "탑건")
+//print(movie.name)
+//
+//movie.name = "탑건:매버릭"
+//print(movie.name)
+
+//struct Movie {
+//        // stored property
+//    var name: String
+//    var director: String
+//
+//        // computed property
+//    var description: String {
+//        return "\(name) is \(director)'s best movie ever"
+//    }
+//}
+//
+//var movie = Movie(name: "인셉션", director: "놀란")
+//print(movie.description)
+
+
+struct Task {
     var name: String
+    var progress: Int {
+        didSet {
+            // print("Current Progress : \(progress) %")
+            var str = ""
+            for i in 1...10 {
+                if progress >= i * 10 {
+                    str += "*"
+                } else {
+                    str += "-"
+                }
+            }
+            print("Current Progress : \(str)")
+        }
+    }
+
+    var isDone: Bool {
+        return progress == 100
+    }
 }
 
 
-var movie = Movie(name: "탑건")
-print(movie.name)
+var task = Task(name: "Very Important Task", progress: 0)
 
-movie.name = "탑건:매버릭"
-print(movie.name)
+task.progress = 30 // Current Progress : 30 %
+task.progress = 50 // Current Progress : 50 %
+task.progress = 90 // Current Progress : 90 %
+task.progress = 100 // Current Progress : 100 %
+task.isDone
